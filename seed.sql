edd7c38dfcda7585085bf38cf70dbb58015031b3
Michael Cui <cuimichael500@gmail.com>
2020-07-17 13:17:07 -0500

    added seeds.sql for inserting values, next step is to display

---
+++ seed.sql
@@ -0,0 +1,20 @@
+USE employeeTrackerDB;
+
+INSERT INTO department (name)
+VALUES ("Engineering");
+INSERT INTO department (name)
+VALUES ("Marketing");
+INSERT INTO department (name)
+VALUES ("Legal");
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 150000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 120000, 1);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Michael", "Cui", 1, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jackie", "Chan", 2, 1);
