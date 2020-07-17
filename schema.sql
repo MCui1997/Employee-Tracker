47483e3a8bf162e331495eef7eb64a89ba9a66e2
Michael Cui <cuimichael500@gmail.com>
2020-07-17 13:08:55 -0500

    added schema creating the 3 tables and can link in node

---
+++ schema.sql
@@ -0,0 +1,34 @@
+-- This Schema will have three tables that consist of  department, role, and then employee
+
+DROP DATABASE IF EXISTS employeeTrackerDB;
+
+CREATE DATABASE employeeTrackerDB;
+
+USE employeeTrackerDB;
+
+-- First table of department
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+
+-- Second table of role
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10.3) NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+-- Last table of employees  
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
+
